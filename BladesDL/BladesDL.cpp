63dacdc3739d3845b2a3e339d2f7beb0a226d704
Mitchell Waite <mitchellwaite@users.noreply.github.com>
2026-03-16 00:21:45 -0400

    Add support for reading plugins from a DashLaunch style launch.ini

--- BladesDL/BladesDL.cpp
+++ BladesDL/BladesDL.cpp
@@ -11,6 +11,9 @@
 
 #include "stdafx.h"
 #include "BladesDL.h"
+#include "INIReader.h"
+
+static INIReader* reader;
 
 // version and launch helper data structure
 extern ldata ldat = {
@@ -22,6 +25,71 @@ extern ldata ldat = {
 	VER_MIN,				// USHORT versionMin;
 	TARGET_KERNEL		// USHORT targetKernel;
 };
+
+void Mount(char* dev, char* mnt)
+{
+	ANSI_STRING asDevice, asMount;
+    RtlInitAnsiString(&asDevice, dev);
+	RtlInitAnsiString(&asMount, mnt);
+	ObCreateSymbolicLink(&asMount, &asDevice);
+}
+
+BOOL LoadPlugins()
+{
+	// Mount all the drives
+	Mount("\\Device\\Harddisk0\\Partition1", "\\System??\\Hdd:");
+	Mount("\\Device\\Mass0", "\\System??\\Usb:");
+	Mount("\\Device\\Mass0", "\\System??\\Mass0:");
+	Mount("\\Device\\Mass1", "\\System??\\Mass1:");
+	Mount("\\Device\\Mass2", "\\System??\\Mass2:");
+	Mount("\\Device\\Flash", "\\System??\\sfcx:");
+
+	reader = new INIReader("Mass0:\\launch.ini");
+	if(reader->ParseError() < 0) reader = new INIReader("Mass1:\\launch.ini");
+	if(reader->ParseError() < 0) reader = new INIReader("Mass2:\\launch.ini");
+	if(reader->ParseError() < 0) reader= new INIReader("Hdd:\\launch.ini");
+	if(reader->ParseError() < 0) reader= new INIReader("sfcx:\\launch.ini");
+	if(reader->ParseError() < 0)
+	{
+		cprintf("[BladesDL] [LoadPlugins] launch.ini not found");
+		return FALSE;
+	}
+
+	// We've got 
+	string temp = reader->Get("Plugins", "plugin1", "NOTFOUND");
+	if(temp!="NOTFOUND" && temp!="none" && temp != ""){
+		cprintf("[BladesDL] [LoadPlugins] plugin1: %s", temp.c_str());
+		if(XexLoadImage(temp.c_str(),8,0,NULL))
+			cprintf("[BladesDL] [LoadPlugins] ERROR: Failed to load %s", temp.c_str());
+	}
+	temp = reader->Get("Plugins", "plugin2", "NOTFOUND");
+	if(temp!="NOTFOUND" && temp!="none" && temp != ""){
+		cprintf("[BladesDL] [LoadPlugins] plugin2: %s", temp.c_str());
+		if(XexLoadImage(temp.c_str(),8,0,NULL))
+			cprintf("[BladesDL] [LoadPlugins] ERROR: Failed to load %s", temp.c_str());
+	}
+	temp = reader->Get("Plugins", "plugin3", "NOTFOUND");
+	if(temp!="NOTFOUND" && temp!="none" && temp != ""){
+		cprintf("[BladesDL] [LoadPlugins] plugin3: %s", temp.c_str());
+		if(XexLoadImage(temp.c_str(),8,0,NULL))
+			cprintf("[BladesDL] [LoadPlugins] ERROR: Failed to load %s", temp.c_str());
+	}
+	temp = reader->Get("Plugins", "plugin4", "NOTFOUND");
+	if(temp!="NOTFOUND" && temp!="none" && temp != ""){
+		cprintf("[BladesDL] [LoadPlugins] plugin4: %s", temp.c_str());
+		if(XexLoadImage(temp.c_str(),8,0,NULL))
+			cprintf("[BladesDL] [LoadPlugins] ERROR: Failed to load %s", temp.c_str());
+	}
+	temp = reader->Get("Plugins", "plugin5", "NOTFOUND");
+	if(temp!="NOTFOUND" && temp!="none" && temp != ""){
+		cprintf("[BladesDL] [LoadPlugins] plugin5: %s", temp.c_str());
+		if(XexLoadImage(temp.c_str(),8,0,NULL))
+			cprintf("[BladesDL] [LoadPlugins] ERROR: Failed to load %s", temp.c_str());
+	}
+
+	cprintf("[BladesDL] [LoadPlugins] complete!");
+	return TRUE;
+}
 
 BOOL WINAPI DllMain(HANDLE hInstDLL, DWORD fdwReason, LPVOID lpReserved)
 {
@@ -40,6 +108,8 @@ BOOL WINAPI DllMain(HANDLE hInstDLL, DWORD fdwReason, LPVOID lpReserved)
 		PatchUpdStrings();
 		ApplyPingPatch();
 		ApplyContentPatch();
+
+		LoadPlugins();
 
 		cprintf("[BladesDL] Init complete!");
 		break;
